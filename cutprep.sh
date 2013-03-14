7ccc43b462629b0925071ed24e62b839050b0045
edyother <ed@edyother.com>
2013-03-14 10:09:54 -0400

    new edit

--- cutprep.sh
+++ cutprep.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
+# This helps me build a script for editing a video with ffmpeg.
 
-#      $0        $1           $2         $3
-# scriptname [songname] [start time] [end time]
-
-# ffmpeg -ss 00:05:57 -t 00:03:00 -i "$I" -acodec copy -vcodec copy 02-hope-above.flv
+#       $0        $1           $2         $3
+# ./cutprep.sh [songname] [start time] [end time]
 
 echo ffmpeg -ss $2 -t $(echo $3-$2 | bc -l) -i '$I' -acodec copy -vcodec copy $1.flv >> cutscript.txt
 echo "" >> cutscript.txt
